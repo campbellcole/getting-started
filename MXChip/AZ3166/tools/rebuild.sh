33456786a70a6b4e24f155dc97ef14b7d1083d16
campbellcole <10430178+campbellcole@users.noreply.github.com>
2022-11-22 17:55:19 -0800

    add patch to rebuild script

--- MXChip/AZ3166/tools/rebuild.sh
+++ MXChip/AZ3166/tools/rebuild.sh
@@ -17,6 +17,9 @@ BUILDDIR="$BASEDIR/build"
 mkdir -p $BUILDDIR
 rm -rf $BUILDDIR/*
 
+# patch the cmake flags for asc_security
+cp $SCRIPTDIR/asc_security_coreBuild.cmake $BASEDIR/../../shared/lib/netxduo/addons/azure_iot/azure_iot_security_module/iot-security-module-core/cmake-modules/
+
 # Generate the build system using Ninja
 cmake -B"$BUILDDIR" -GNinja -DCMAKE_TOOLCHAIN_FILE=$BASEDIR/../../cmake/arm-gcc-cortex-m4.cmake $BASEDIR
 
